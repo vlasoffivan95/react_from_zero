37de0e6d158bd0b29f8cb91d668a3c8d959c0298
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-04-09 23:23:07 +0300

    input focus example

--- src/components/RefClicker/index.jsx
+++ src/components/RefClicker/index.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useClicker } from "hooks";
 import { useRef } from "react";
 
 const RefClicker = () => {
   const elemRef = useRef(null);
   const clicks = useClicker(elemRef);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [inputRef]);
+
   return (
     <div>
       <h1>Clicks: {clicks}</h1>
+      <input type="text" ref={inputRef}></input>
       <div
         ref={elemRef}
         style={{
