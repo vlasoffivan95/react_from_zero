cc1378ee000c18dcfc61703a2dfb15fff523447b
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-04-11 23:03:10 +0300

    reexport done

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,2 @@
-import useClicker from "./useClicker";
-import useData from "./useData";
-
-export {useClicker, useData}
+export { default as useClicker } from "./useClicker";
+export { default as useData } from "./useData";
