19de23cd60acbe67f76e121d9d6755f1c7453451
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-04-24 22:29:16 +0300

    validate message example

--- src/utils/validators/validationSchema.js
+++ src/utils/validators/validationSchema.js
@@ -1,7 +1,15 @@
-import * as yup from "yup"
+import * as yup from "yup";
+
+const EMAIL_SCHEMA = yup.string().email().required()
+
 export const SIGN_UP_SCHEMA = yup.object({
-    email: yup.string().email().required(),
-    firstName: yup.string().min(1).max(128).required(),
-    lastName: yup.string().min(1).max(128).required(),
-    password: yup.string().min(8).required(),
-  });
+  email: EMAIL_SCHEMA,
+  firstName: yup.string().min(1).max(128).required(),
+  lastName: yup.string().min(1).max(128).required('You must enter last name'),
+  password: yup.string().matches(/[A-Za-z]{8,}/, 'Password must be a 8 symbol or more letters only').required(),
+});
+
+export const LOGIN_SCHEMA = yup.object({
+  email: EMAIL_SCHEMA,
+  password: yup.string().min(8).required(),
+});
