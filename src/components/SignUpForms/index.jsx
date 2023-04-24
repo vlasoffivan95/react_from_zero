f07f86511416afacc4a745e4a177777684beb347
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-04-24 21:31:24 +0300

    add yup

--- src/components/SignUpForms/index.jsx
+++ src/components/SignUpForms/index.jsx
@@ -1,104 +1,43 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import styles from "./SignUpForm.module.scss";
+import * as yup from "yup";
 
-function signUp(userData) {
-  console.log(`${userData.email} ${userData.password} is registered`);
-}
+const SIGN_UP_SCHEMA = yup.shape({
+  email: yup.string().required(),
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  password: yup.string().required(),
+});
 
-const initialState = {
-  email: "",
-  password: "",
-  accountLevel: "basic",
-  isAdult: false,
-  gender: 'female'
-};
-
-class SignUpForm extends Component {
-  state = {
-    ...initialState,
-  };
-
-  submitHandler = (e) => {
-    const { email, password } = this.state;
+function SignUpForm(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const submitHandler = (e) => {
     e.preventDefault();
-    signUp({
-      email,
-      password,
-    });
-    this.setState({
-      ...initialState,
-    });
-  };
-  handleEmailChange = (e) => {
-    this.setState({
-      email: e.target.value,
-    });
   };
 
-  handlePasswordChange = (e) => {
-    this.setState({
-      password: e.target.value,
-    });
-  };
+  return (
+    <form onSubmit={submitHandler} className={styles.form}>
+      <input
+        className={styles.input}
+        type="email"
+        name="email"
+        value={email}
+        onChange={({ target: { value } }) => setEmail(value)}
+      ></input>
+      <input
+        className={styles.input}
+        type="password"
+        name="password"
+        value={password}
+        onChange={({ target: { value } }) => setPassword(value)}
+      ></input>
 
-  handleChange = (e) => {
-    const {
-      target: { value, name, type, checked },
-    } = e;
-    const newValue = type === "checkbox" ? checked : value;
-    this.setState({
-      [name]: newValue,
-    });
-  };
-
-  render() {
-    const { email, password, accountLevel, isAdult, gender } = this.state;
-    return (
-      <form onSubmit={this.submitHandler} className={styles.form}>
-        <input
-          className={styles.input}
-          type="email"
-          name="email"
-          value={email}
-          onChange={this.handleChange}
-        ></input>
-        <input
-          className={styles.input}
-          type="password"
-          name="password"
-          value={password}
-          onChange={this.handleChange}
-        ></input>
-        <select
-          className={styles.input}
-          name="accountLevel"
-          value={accountLevel}
-          onChange={this.handleChange}
-        >
-          <option value="basic">Basic</option>
-          <option value="adv">Adv</option>
-          <option value="pro">Pro</option>
-        </select>
-        <label>
-          <input
-            className={styles.checkbox}
-            type="checkbox"
-            name="isAdult"
-            checked={isAdult}
-            onChange={this.handleChange}
-          />{" "}
-          Are you an adult?
-        </label>
-        <label>
-          <input type="radio" name="gender" value="male" onChange={this.handleChange} checked={gender==='male'}/> Male
-          <input type="radio" name="gender" value="female" onChange={this.handleChange} checked={gender==='female'} /> Female
-        </label>
-        <button disabled={!isAdult} className={styles.btn} type="submit">
-          Sign Up
-        </button>
-      </form>
-    );
-  }
+      <button className={styles.btn} type="submit">
+        Sign Up
+      </button>
+    </form>
+  );
 }
 
 export default SignUpForm;
