61ff65537589a4b58f2487a707a3531ad70c102b
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-04-25 22:55:01 +0300

    add form example

--- src/components/SignUpForms/index.jsx
+++ src/components/SignUpForms/index.jsx
@@ -1,44 +1,47 @@
 import React from "react";
-import { Formik } from "formik";
+import { Formik, Field } from "formik";
 import styles from "./SignUpForm.module.scss";
 
 const initialState = {
   email: "",
   password: "",
   name: "",
+  gender: "male",
+  isSub: false,
+  accountLevel: "basic",
 };
 
 function SignUpForm(props) {
-  const submitHandler = (values,formikBag) => {
-    console.log(values)
-    console.log(formikBag)
+  const submitHandler = (values, formikBag) => {
+    console.log(values);
+    console.log(formikBag);
   };
 
   return (
     <Formik initialValues={initialState} onSubmit={submitHandler}>
       {(formikProps) => {
-        console.log(formikProps);
         return (
           <form className={styles.form} onSubmit={formikProps.handleSubmit}>
-            <input
-              className={styles.input}
-              type="email"
-              name="email"
-              onChange={formikProps.handleChange}
-            ></input>
-            <input
-              className={styles.input}
-              type="password"
-              name="password"
-              onChange={formikProps.handleChange}
-            ></input>
-
-            <input
-              className={styles.input}
-              type="text"
-              name="name"
-              onChange={formikProps.handleChange}
-            ></input>
+            <Field className={styles.input} type="email" name="email" />
+            <Field className={styles.input} type="password" name="password" />
+            <Field className={styles.input} type="text" name="name" />
+            <Field name="accountLevel" as="select" >
+              <option value="basic">Basic</option>
+              <option value="middle">Middle</option>
+              <option value="Pro">Pro</option>
+              </Field>
+            <label>
+              <Field type="radio" name="gender" value="male" />
+              Male
+            </label>
+            <label>
+              <Field type="radio" name="gender" value="female" />
+              Female
+            </label>
+            <label>
+              <Field type="checkbox" name="isSuB" />
+              Add Sub
+            </label>
 
             <button className={styles.btn} type="submit">
               Sign Up
