00ab4790409d5c76db36c6e76113d9d7c28b1d4d
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-04-25 23:13:12 +0300

    add validator

--- src/components/SignUpForms/index.jsx
+++ src/components/SignUpForms/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Formik, Field, Form } from "formik";
 import styles from "./SignUpForm.module.scss";
+import { SIGN_UP_SCHEMA } from "utils/validators/validationSchema";
 
 const initialState = {
   email: "",
@@ -15,14 +16,20 @@ function SignUpForm(props) {
   const submitHandler = (values, formikBag) => {
     console.log(values);
     console.log(formikBag);
+
+    formikBag.resetForm();
   };
 
   return (
-    <Formik initialValues={initialState} onSubmit={submitHandler}>
+    <Formik
+      initialValues={initialState}
+      onSubmit={submitHandler}
+      validationSchema={SIGN_UP_SCHEMA}
+    >
       <Form className={styles.form}>
-        <Field className={styles.input} type="email" name="email" />
-        <Field className={styles.input} type="password" name="password" />
-        <Field className={styles.input} type="text" name="name" />
+        <Field className={styles.input} type="email" name="email" placeholder="email"/>
+        <Field className={styles.input} type="password" name="password" placeholder="password" />
+        <Field className={styles.input} type="text" name="name" placeholder="name"/>
         <Field name="accountLevel" as="select">
           <option value="basic">Basic</option>
           <option value="middle">Middle</option>
--- src/utils/validators/validationSchema.js
+++ src/utils/validators/validationSchema.js
@@ -1,12 +1,17 @@
 import * as yup from "yup";
 
-const EMAIL_SCHEMA = yup.string().email().required()
+const EMAIL_SCHEMA = yup.string().email().required();
 
 export const SIGN_UP_SCHEMA = yup.object({
   email: EMAIL_SCHEMA,
-  firstName: yup.string().min(1).max(128).required(),
-  lastName: yup.string().min(1).max(128).required('You must enter last name'),
-  password: yup.string().matches(/[A-Za-z]{8,}/, 'Password must be a 8 symbol or more letters only').required(),
+  name: yup.string().min(1).max(128).required(),
+  password: yup
+    .string()
+    .matches(/[A-Za-z]{8,}/, "Password must be a 8 symbol or more letters only")
+    .required(),
+  gender: yup.string().required(),
+  isSub: yup.bool(),
+  accountLevel: yup.string().required(),
 });
 
 export const LOGIN_SCHEMA = yup.object({
